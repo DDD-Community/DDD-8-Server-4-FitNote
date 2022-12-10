81776ecc7f2a64ab65eefb37d0d5865e4daa7cc8
integerJI <jjs9536@gmail.com>
2022-12-10 13:11:52 +0900

    Feat: json 셋팅

--- members/views.py
+++ members/views.py
@@ -17,32 +17,46 @@ import json
 @authentication_classes((JWTAuthentication,))
 def list(request):
     """
-        회원 리스트 호출
+        회원 리스트 호출 : 피그마 운동 일지 회원 목록
         ---
         # 내용
-            - title : 영화제목
-            - year : 영화 출시 년도
-            - genre : 영화 장르
+            - status    : 응답 상태
+            - code      : 응답 코드
+            - message   : 응답 메시지
+            - data      : model : DB 테이블 명, pk : 고유 키, fields : 데이터
     """
     member_list = serializers.serialize('json', User.objects.filter(user_type=1).order_by('-id'), fields=('user_name'))
     member_json = json.loads(member_list)
 
     response = {}
-    response["result"] = "false"
-    response["error_code"] = "301"
-    response["message"] = "잘못된 요청입니다."
-    response["return_url"] = "/"
+    response["status"] = "true"
+    response["code"] = "200"
+    response["message"] = "정상 요청"
     response["data"] = member_json
     return JsonResponse(response, json_dumps_params = {'ensure_ascii': False})
-
-    # print(member_json[0]['fields'])
-    # return HttpResponse(member_json, content_type="text/json-comment-filtered")
 
 @api_view(['GET'])
 @permission_classes((IsAuthenticated,))
 @authentication_classes((JWTAuthentication,))
-class detail(APIView):
-    def get(self, request):
-        member_detail = serializers.serialize('json', User.objects.filter(user_type=1).order_by('-id'), fields=('user_name', 'create_data', 'user_gender', 'user_height', 'user_weight'))
-        return HttpResponse(member_detail, content_type="text/json-comment-filtered")
+def detail(request):
+    """
+        회원 리스트 호출
+        ---
+        # 내용
+            - status    : 응답 상태
+            - code      : 응답 코드
+            - message   : 응답 메시지
+            - data      : model : DB 테이블 명, pk : 고유 키, fields : 데이터
+    """
+    user_name = request.GET['userName']
+    member_detail = serializers.serialize('json', User.objects.filter(user_type=1, user_name=user_name).order_by('-id'), fields=('user_name', 'create_data', 'user_gender', 'user_height', 'user_weight'))
+    member_json = json.loads(member_detail)
+
+    response = {}
+    response["status"] = "true"
+    response["code"] = "200"
+    response["message"] = "정상 요청"
+    response["data"] = member_json
+
+    return JsonResponse(response, json_dumps_params = {'ensure_ascii': False})
 
