bc802bdab5a18afb9842aaa756873a1a52ceb38a
integerJI <jjs9536@gmail.com>
2023-01-29 15:52:41 +0900

    Remove: 불필요한 APP 제거

--- customuser/settings.py
+++ customuser/settings.py
@@ -137,8 +137,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'members',
-    'notes',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_simplejwt.token_blacklist',
--- notes/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('index/', views.index, name='index'),
-    path('data/', views.getApi, name='data'),
-]
